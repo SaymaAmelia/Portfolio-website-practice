64168447f9faf6078cc0274d3bfc090f3edcf45d
SaymaAmelia <saymaamelia@gmail.com>
2025-04-26 09:50:52 +0600

    Contact_html_part done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/About'
 import Skill from './components/Skill'
 import Work from './components/Work'
 import Review from './components/Review'
+import Contact from './components/Contact'
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Skill/>
         <Work/>
        <Review/> 
+       <Contact/>
        </main>
     </>
   )
