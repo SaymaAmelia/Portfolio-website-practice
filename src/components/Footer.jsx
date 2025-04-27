eff837e17567752fe144dfa9f5123317184798ac
SaymaAmelia <saymaamelia@gmail.com>
2025-04-27 10:08:02 +0600

    footer part done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -52,15 +52,52 @@ const sitemap = [
      return (
        <footer className='section'>
          <div className='container'>
-            <div>
-                <h2 className='heading-1'>
-                Let&apos;s work together today!
+          <div className='lg:grid lg:grid-cols-2'>
+
+            <div className='mb-10'>
+                <h2 className='heading-1 mb:8 lg:max-w-[12ch] '>
+                     Let&apos;s work together today!
                 </h2>
                 <ButtonPrimary
                  href="mailto:saymaamelia@gmail.com"
                    label="start project"
                    icon="chevron_right"/>
             </div>
+
+            <div className='grid grid-cols-2 gap-4 lg:pl-20'>
+
+              <div className=''>
+                <p className='mb-2'>sitemap</p>
+                <ul>
+                  {sitemap.map(({label, href}, key)=>(
+                    <li key={key}>
+                      <a href={href}
+                      className='block text-sm text-zinc-400 py-1 hover:text-zinc-200 transition-colors duration-300'>
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div>
+                <p className='mb-2'>socials</p>
+                <ul>
+                  {socials.map(({label, href}, key)=>(
+                    <li key={key}>
+                      <a href={href}
+                      className='block text-sm text-zinc-400 py-1 hover:text-zinc-200 transition-colors duration-300'>
+                        {label}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              
+            </div>
+
+          </div>
 
          </div>
 
